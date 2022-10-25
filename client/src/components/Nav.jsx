0ff7c3be8b75c0e360d9c26b79759e98e7531e5f
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-10-25 12:15:18 -0400

    Created Nav links

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,8 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import Home from './Home'
+import EventsList from './EventsList'
+import PhotosList from './PhotosList'
+import AddPhotoForm from './AddPhotoForm';
+import AddEventForm from './AddEventForm';
+
 const Nav = () => {
 
     return (
         <div>
             <h1>Nav</h1>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/events" >Events</NavLink>
+            <NavLink to="/photos" >Photos</NavLink>
+            <NavLink to="/add/event" >Add Event</NavLink>
+            <NavLink to="/add/photo" >Add Photo</NavLink>
         </div>
     )
 }
