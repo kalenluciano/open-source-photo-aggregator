24906cf5d6427401a83cd7daa200af1af48dace7
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-10-25 12:36:21 -0400

    Successful first axios call

--- client/src/components/EventsList.jsx
+++ client/src/components/EventsList.jsx
@@ -1,5 +1,18 @@
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+
 const EventsList = () => {
-    
+    const [events, setEvents] = useState([])
+
+    const getEvents = async () => {
+        const response = await axios.get('http://localhost:3001/news-events')
+        console.log(response)
+    }
+
+    useEffect(() => {
+        getEvents()
+    }, [])
+
     return (
         <div>
             <h1>Events List</h1>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,11 @@
+import {NavLink} from 'react-router-dom'
+
 const Home = () => {
 
     return (
         <div>
             <h1>Home</h1>
+            <NavLink to="/events" >Click to Events List</NavLink>
         </div>
     )
 }
