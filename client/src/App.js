f317e25ef5d4b78f61e5b4be232b42110f4847e8
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-10-25 10:27:15 -0400

    Created first few components and rendered them in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Nav from './components/Nav';
+import Landing from './components/Landing';
+import Footer from './components/Footer';
 
-function App() {
-	return <div className="App"></div>;
-}
+const App = () => {
+	return (
+		<div className="App">
+			<Nav />
+			<Landing />
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
