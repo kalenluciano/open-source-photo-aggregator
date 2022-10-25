79f57d65defa6eca4fc335937972ba5eb24b7196
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-10-25 10:34:27 -0400

    Created initial routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 import Landing from './components/Landing';
@@ -7,7 +8,9 @@ const App = () => {
 	return (
 		<div className="App">
 			<Nav />
-			<Landing />
+			<Routes>
+				<Route path="/" element={<Landing />} />
+			</Routes>
 			<Footer />
 		</div>
 	);
