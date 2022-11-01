dab5cc954f6655b62e6373d61026f95e9f9ee772
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-10-31 21:09:23 -0400

    Fixed error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const db = require('./db');
 const cors = require('cors');
 const logger = require('morgan');
-const PORT = process.env.MONGODB_URI || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
