2ee0e205ec51ca8b3864d717c4deb4e0fe6b4002
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-10-31 22:00:02 -0400

    Maybe fixed heroku issue

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,1 +1,4 @@
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL =
+	location.hostname === 'localhost'
+		? 'http://localhost:3001'
+		: 'https://open-source-photo-aggregator.herokuapp.com';
